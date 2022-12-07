7d4a6a4a741f106de3dfcb77c319308807b1a613
Boyou Han <boyouh2@illinois.edu>
2022-12-06 19:28:35 -0600

    idk

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 8014;
+app.use(body_parser_1.default.json());
 app.post("/webhook", (req, res) => {
     console.log(req);
+    console.log(req.params);
+    console.log(req.body);
 });
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8014;
 
+app.use(bodyParser.json())
 
 app.post( "/webhook", ( req, res ) => {
   console.log(req);
