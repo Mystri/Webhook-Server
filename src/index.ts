7ea30c1ab585a0554719bcec4edaa9a1f8b92101
Boyou Han <boyouh2@illinois.edu>
2022-12-06 19:02:42 -0600

    change to console dir

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const port = 8014;
 
+
 app.post( "/webhook", ( req, res ) => {
   console.dir(req.body);
+  console.log(req.body);
 } );
 
 app.listen( port, () => {
